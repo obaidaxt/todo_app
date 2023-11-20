fb4c22d8594b51d5a81eb3295b4132f1ddf5360b
obaidaxt <obaidaxt@gmail.com>
2023-11-20 09:39:00 +0100

    2011

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,189 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:todo_app/widgets/counter.dart';
+import 'package:todo_app/widgets/todo_card.dart';
 
-class Counter extends StatelessWidget {
-  final int allTodos;
-  final int allCompleted;
+void main() {
+  runApp(const MyApp());
+}
 
-  const Counter({Key? key, required this.allCompleted, required this.allTodos})
-      : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 27),
-      child: Text("$allCompleted/$allTodos",
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TodoApp(),
+    );
+  }
+}
+
+class TodoApp extends StatefulWidget {
+  const TodoApp({Key? key}) : super(key: key);
+
+  @override
+  State<TodoApp> createState() => _TodoAppState();
+}
+
+// class for task(todo-card)
+class Task {
+  String title;
+  bool status;
+  Task({
+    required this.title,
+    required this.status,
+  });
+}
+
+class _TodoAppState extends State<TodoApp> {
+  // list of todos
+  List allTasks = [
+    Task(title: "Publish video", status: true),
+    Task(title: "Laugh louder", status: true),
+    Task(title: "GEM", status: true),
+    Task(title: "call mom", status: true),
+  ];
+
+// To remove todo  when clicking on "delete" icon
+  delete(int clickedTask) {
+    setState(() {
+      allTasks.remove(allTasks[clickedTask]);
+    });
+  }
+
+// To remove all todos  when clicking on "delete" icon in the appBar
+  deleteAll() {
+    setState(() {
+      allTasks.removeRange(0, allTasks.length);
+    });
+  }
+
+// To change the state of the todo (completed or not completed) when click on the todo
+  changeStatus(int taskIndex) {
+    setState(() {
+      allTasks[taskIndex].status = !allTasks[taskIndex].status;
+    });
+  }
+
+// To add new todo when clicking on "ADD" in the dialog widget
+  addnewtask() {
+    setState(() {
+      allTasks.add(
+        Task(title: myController.text, status: false),
+      );
+    });
+  }
+
+// Create controller to  get the text inside the textfield  in the dialog widget
+  final myController = TextEditingController();
+
+// To calculate only completed todos
+// we will explain the difference between forEach & for loop in the next lesson (lesson11)
+  int calculateCompletedTasks() {
+    int completedTasks = 0;
+
+    for (var item in allTasks) {
+      if (item.status) {
+        completedTasks++;
+      }
+    }
+
+    return completedTasks;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return Dialog(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(11)),
+                child: Container(
+                  padding: EdgeInsets.all(22),
+                  height: 200,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextField(
+                          controller: myController,
+                          maxLength: 20,
+                          decoration:
+                              InputDecoration(hintText: "Add new Task")),
+                      SizedBox(
+                        height: 22,
+                      ),
+                      TextButton(
+                          onPressed: () {
+                            addnewtask();
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            "ADD",
+                            style: TextStyle(fontSize: 22),
+                          ))
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+        backgroundColor: Colors.redAccent,
+        child: Icon(Icons.add),
+      ),
+      backgroundColor: Color.fromRGBO(58, 66, 86, 0.7),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              deleteAll();
+            },
+            icon: Icon(Icons.delete_forever),
+            iconSize: 37,
+            color: Color.fromARGB(255, 255, 188, 214),
+          )
+        ],
+        elevation: 0,
+        backgroundColor: Color.fromRGBO(58, 66, 86, 1),
+        title: Text(
+          "TO DO APP",
           style: TextStyle(
-              fontSize: 44,
-              color: allCompleted == allTodos
-                  ? Color.fromARGB(255, 142, 253, 200)
-                  : Colors.white,
-              fontWeight: FontWeight.bold)),
+              fontSize: 33, color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Counter(
+                allTodos: allTasks.length,
+                allCompleted: calculateCompletedTasks()),
+            Container(
+              margin: EdgeInsets.only(top: 22),
+              height: 550,
+              color: Color.fromARGB(255, 55, 63, 82),
+              child: ListView.builder(
+                  itemCount: allTasks.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Todecard(
+                        // I will pass all these information when create the Todecard() widget in "todo-card.dart" file
+                        vartitle: allTasks[index].title,
+                        doneORnot: allTasks[index].status,
+                        changeStatus: changeStatus,
+                        index: index,
+                        delete: delete);
+                  }),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/widgets/todo_card.dart
+++ lib/widgets/todo_card.dart
@@ -5,32 +5,71 @@ import 'package:flutter/material.dart';
 class Todecard extends StatelessWidget {
   final String vartitle;
   final bool doneORnot;
-  const Todecard({Key? key, required this.vartitle, required this.doneORnot})
+  final Function changeStatus;
+  final int index;
+  final Function delete;
+
+  const Todecard(
+      {Key? key,
+      required this.vartitle,
+      required this.changeStatus,
+      required this.index,
+      required this.delete,
+      required this.doneORnot})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      widthFactor: 0.9,
-      child: Container(
-        margin: EdgeInsets.only(top: 20),
-        padding: EdgeInsets.all(22),
-        decoration: BoxDecoration(
-            color: Color.fromRGBO(209, 224, 224, 0.2),
-            borderRadius: BorderRadius.circular(11)),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              vartitle,
-              style: TextStyle(color: Colors.white, fontSize: 22),
-            ),
-            Icon(
-              doneORnot ? Icons.check : Icons.close,
-              size: 27,
-              color: doneORnot ? Colors.green[400] : Colors.red,
-            )
-          ],
+    return GestureDetector(
+      onTap: () {
+        changeStatus(index);
+      },
+      child: FractionallySizedBox(
+        widthFactor: 0.9,
+        child: Container(
+          margin: EdgeInsets.only(top: 20),
+          padding: EdgeInsets.all(15),
+          decoration: BoxDecoration(
+              color: Color.fromRGBO(209, 224, 224, 0.2),
+              borderRadius: BorderRadius.circular(11)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                vartitle,
+                style: TextStyle(
+                  //  condition? true : false
+                  color: doneORnot
+                      ? Color.fromARGB(255, 27, 26, 26)
+                      : Colors.white,
+                  fontSize: 22,
+                  decoration: doneORnot
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                ),
+              ),
+              Row(
+                children: [
+                  Icon(
+                    doneORnot ? Icons.check : Icons.close,
+                    size: 27,
+                    color: doneORnot ? Colors.green[400] : Colors.red,
+                  ),
+                  SizedBox(
+                    width: 17,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      delete(index);
+                    },
+                    icon: Icon(Icons.delete),
+                    iconSize: 27,
+                    color: Color.fromARGB(255, 255, 200, 196),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
